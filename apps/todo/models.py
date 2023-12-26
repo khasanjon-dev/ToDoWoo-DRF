a408c3491a843f34176c48af5ba428cfed77172b
Khasanjon Mamadaliyev <khasanjon.dev@gmail.com>
2023-12-26 11:57:18 +0500

    ready some files

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Model, CharField, TextField, BooleanField, DateTimeField, ForeignKey, CASCADE
 
-# Create your models here.
+
+class ToDoWoo(Model):
+    title = CharField(max_length=300)
+    description = TextField()
+    # bool
+    is_important = BooleanField(default=False)
+    is_complete = BooleanField(default=False)
+    # date
+    created_at = DateTimeField(auto_now_add=True, blank=True)
+    updated_at = DateTimeField(auto_now=True, null=True, blank=True)
+    completed_at = DateTimeField(null=True, blank=True)
+    # relationship
+    author = ForeignKey(User, CASCADE, 'todos')
--- root/settings.py
+++ root/settings.py
@@ -1,22 +1,15 @@
+import os.path
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv
+
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e^5etojon7$70v=kd)a056(kfauq7u_54ch)fi8#c&cixi-fdr'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -25,6 +18,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    'rest_framework',
+
+    # my apps
+    'users',
+    'todo'
 ]
 
 MIDDLEWARE = [
@@ -57,20 +56,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'root.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'todo.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -87,25 +78,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
